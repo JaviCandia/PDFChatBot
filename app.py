37207fcfc74ba7584d9828e18bcdad348d07f4d1
Javier Candia <javi-candia@hotmail.com>
2024-11-20 06:28:18 -0600

    knowledge base stored in session

--- app.py
+++ app.py
@@ -19,6 +19,10 @@ st.header("Ask questions about your PDF")
 if 'embeddings' not in st.session_state: 
     st.session_state.embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
 
+# Initialize global knowledge base
+if 'knowledge_base' not in st.session_state:
+    st.session_state.knowledge_base = None
+
 # Upload a PDF file
 pdf_obj = st.file_uploader("Upload your PDF", type="pdf", on_change=st.cache_resource.clear)
 
@@ -46,12 +50,12 @@ def create_embeddings(pdf):
 
 # If a PDF file is uploaded
 if pdf_obj:
-    knowledge_base = create_embeddings(pdf_obj)
+    st.session_state.knowledge_base = create_embeddings(pdf_obj)
     user_question = st.text_area("Ask something about your PDF:")
 
     if user_question:
         os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
-        docs = knowledge_base.similarity_search(user_question, 3)
+        docs = st.session_state.knowledge_base.similarity_search(user_question, 3)
 
         llm = ChatOpenAI(model_name='gpt-3.5-turbo')
         chain = load_qa_chain(llm, chain_type="stuff")
