e5c7647c791b2aadd9868c1d7807469462aed7f9
Javier Candia <javi-candia@hotmail.com>
2024-11-28 17:04:57 -0600

    Single CV against multiple roles implementation

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-
+import json
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
@@ -10,25 +10,30 @@ from langchain.schema import Document
 
 from langchain_core.prompts import PromptTemplate
 
-from output_parsers import feedback_parser
+from output_parsers import feedback_parser, RoleMatch
 
 # Load environment variables from .env file
 load_dotenv()
 
 # Streamlit app configuration
-st.set_page_config('CV Scanner MVP')
+st.set_page_config("CV Scanner MVP")
 st.header("CV Scanner MVP")
 
 # Initialize global embeddings model
-if 'embedding_model' not in st.session_state:
-    st.session_state.embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
+if "embedding_model" not in st.session_state:
+    st.session_state.embedding_model = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+    )
 
 # Preinitialize the LLM
-if 'llm' not in st.session_state:
-    st.session_state.llm = ChatOpenAI(model_name='gpt-4o-mini', temperature=0)
+if "llm" not in st.session_state:
+    st.session_state.llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
 
 # Upload a PDF file
-pdf_file = st.file_uploader("Upload your CV", type="pdf", on_change=st.cache_resource.clear)
+pdf_file = st.file_uploader(
+    "Upload your CV", type="pdf", on_change=st.cache_resource.clear
+)
+
 
 @st.cache_resource
 def create_document(pdf):
@@ -42,51 +47,62 @@ def create_document(pdf):
     document = Document(page_content=text, metadata={})
     return [document]
 
+# Load roles from JSON file
+with open("roles.json", "r", encoding="utf-8") as file:
+    roles = json.load(file)
+
 # If a PDF file is uploaded
 if pdf_file:
     documents = create_document(pdf_file)
-    job_description = st.text_area("Provide the job description:")
 
-    if job_description:
-        os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
+    if roles:
+        os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
         new_match_prompt = PromptTemplate(
-            input_variables=["documents", "job_description"],
-            partial_variables={"format_instructions": feedback_parser.get_format_instructions},
+            input_variables=["documents", "roles"],
+            partial_variables={
+                "format_instructions": feedback_parser.get_format_instructions
+            },
             template="""
                 Based on the provided CV information: {documents}
-                and the job position details: {job_description},
+                and the following roles: {roles},
 
                 Please perform the following tasks:
 
                 1. **List the Candidate's Main Skills and Experiences**:
                 - Identify and enumerate the 5 primary skills and experiences mentioned in the CV.
 
-                2. **Candidate Skills that fit Job Position**:
-                - Provide a list of 5 skills from the candidate's CV that directly fit the job position.
-                - If there are no relevant skills, indicate with a single bullet:
-                    * There are no skills that fit the job position.
+                2. **Role Match**:
+                - For each role, provide:
+                    - The role name.
+                    - A list of skills from the candidate's CV (not the main skills) that fit the role (5 maximum).
+                    - If there are no relevant skills, indicate with a single bullet:
+                        * There are no skills that fit the job position.
+                    - A match score from 0 to 100 indicating how well the candidate fits the role.
 
-                3. **Match Score: -right here provide a match score from 0 to 100 indicating how well the candidate fits the job position-**
-                - Use 0 if the job position has nothing to do with the candidate's skills or is in a completely different field.
-                - Justify the match score by highlighting key similarities or discrepancies between the candidate's qualifications and the job requirements.
                 \n{format_instructions}
-            """
+            """,
         )
 
         chain = new_match_prompt | st.session_state.llm | feedback_parser
-        res = chain.invoke(input = {"documents": documents, "job_description": job_description})
+        res = chain.invoke(input={"documents": documents, "roles": roles})
 
-        st.write("### Mains Skills of the candidate")
+        st.write("## Main Skills of the candidate")
         st.markdown("\n".join([f"- {skill}" for skill in res.main_skills]))
 
-        st.write("### Skills that fit the job position")
-        if res.fit_skills:
-            st.markdown("\n".join([f"- {skill}" for skill in res.fit_skills]))
-        else:
-            st.markdown("- There are no skills that fit the job position")
 
-        st.write(f"### Match Score: {res.match_score}%")
+        # Dios que haz que acabe esto ya
+        st.write("## Role Match")
+        for role_match in res.role_matches:
+            st.write(f"#### Name: {role_match.rol_name}")
+            st.write("Skills that fit the role:")
+            if role_match.fit_skills:
+                st.markdown(
+                    "\n".join([f"- {skill}" for skill in role_match.fit_skills])
+                )
+            else:
+                st.markdown("- There are no skills that fit the job position")
+            st.write(f"**Match Score: {role_match.match_score}%**")
 
         # Demonstration purposes:
         # st.write("### Response:")
--- output_parsers.py
+++ output_parsers.py
@@ -1,13 +1,20 @@
-from typing import List, Dict
+from typing import List, Dict, Any
 from langchain_core.output_parsers import PydanticOutputParser
 from pydantic import BaseModel, Field
 
+class RoleMatch(BaseModel):
+    rol_name: str = Field(description="Name of the role")
+    fit_skills: List[str] = Field(description="Skills from the candidate that fit the role")
+    match_score: int = Field(description="Score from 0 to 100 indicating how the candidate fits the role")
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {"rol_name": self.rol_name, "fit_skills": self.fit_skills, "match_score": self.match_score}
+
 class FeedbackModel(BaseModel):
     main_skills: List[str] = Field(description="Main skills from the candidate CV")
-    fit_skills: List[str] = Field(description="Skills from the candidate that fit the job position")
-    match_score: int = Field(description="Score from 0 to 100 indicating how the candidate fits the job position")
+    role_matches: List[RoleMatch] = Field(description="List of role matches with the candidate")
 
-    def to_dict(self) -> Dict[str, any]:
-        return {"main_skills": self.main_skills, "fit_skills": self.fit_skills, "match_score": self.match_score}
-    
+    def to_dict(self) -> Dict[str, Any]:
+        return {"main_skills": self.main_skills, "role_matches": [role_match.to_dict() for role_match in self.role_matches]}
+
 feedback_parser = PydanticOutputParser(pydantic_object=FeedbackModel)
